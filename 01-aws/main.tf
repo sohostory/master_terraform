354d74d65fdfc3802451a03bcabdd497f6789b0b
sohostory <sohostory@gmail.com>
2024-05-20 21:50:26 -0500

    configuring aws_subnet resource

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -12,10 +12,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "Main VPC"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "web" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "Web Subnet"
   }
 }
