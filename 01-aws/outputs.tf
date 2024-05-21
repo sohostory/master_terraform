70909f4de9f7ff49d6ac44eeb8f0d823dfe56a64
Hoyoung Lee <sohostory@gmail.com>
2024-05-21 12:08:54 -0500

    created outputs file to log ip and id

---
+++ 01-aws/outputs.tf
@@ -0,0 +1,14 @@
+output "ec2_public_ip" {
+  description = "The public IP address of the EC2 instance."
+  value = aws_instance.my_vm.public_ip
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value = aws_vpc.main.id
+}
+
+output "ami_id" {
+  description = "ID of the AMI"
+  value = aws_instance.my_vm.ami
+}
