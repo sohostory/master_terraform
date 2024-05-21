1f8ae7b97dfe9a2cec1891fa152784e191efc4db
sohostory <sohostory@gmail.com>
2024-05-20 22:45:37 -0500

    setup default security group and firewall configurations

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -50,3 +50,30 @@ resource "aws_default_route_table" "main_vpc_default_rt" {
     Name = "my-default-rt"
   }
 }
+
+# Create a default security group
+resource "aws_default_security_group" "default_sec_group" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80 
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Default Security Group"
+  }
+}
