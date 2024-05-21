3fdc4fea0651755eb3726097954a184eb7ceea6b
sohostory <sohostory@gmail.com>
2024-05-20 21:34:19 -0500

    practicing destory with targeting

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -16,6 +16,14 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    "Name" = "Main VPC"
+    Name = "Main VPC"
+  }
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "192.168.0.0/16"
+
+  tags = {
+    Name = "My VPC"
   }
 }
